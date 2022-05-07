e51ab98a26cab7e7d0ff8595ff462d4bc32706f4
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-05-06 17:27:29 -0700

    fliter

--- playlist/app/app.js
+++ playlist/app/app.js
@@ -1,5 +1,28 @@
 let MyPlayListApp = angular.module('myPlayList', []);
 
-MyPlayListApp.controller('PlaylistController', function($scope) {
+MyPlayListApp.controller('PlaylistController',["$scope", function($scope) {
   $scope.message = "Hello AngularJS";
-});
+
+  $scope.playlists = [
+    {
+      name: "First-song",
+      artist: "Fhun",
+      number: 1,
+    },
+    {
+      name: "Second-song",
+      artist: "Shen",
+      number: 2,
+    },
+    {
+      name: "Third-song",
+      artist: "Tom",
+      number: 3,
+    },
+    {
+      name: "Forth-song",
+      artist: "Funky guy",
+      number: 4,
+    }
+  ];
+}]);
