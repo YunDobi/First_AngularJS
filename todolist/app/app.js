a091de170c3d68dd72f7f5ed4fd73ea7bda4fbe8
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-05-09 18:54:36 -0700

    playlist todolist

--- todolist/app/app.js
+++
@@ -1,9 +0,0 @@
-let todoListapp = angular.module('todolsits', []);
-
-todoListapp.controller('todolistController', ['$scope', function($scope) {
-  $scope.todos = ['first todo'];
-
-  $scope.addingTask = function(task) {
-    $scope.todos.push(task);
-  };
-}]);
